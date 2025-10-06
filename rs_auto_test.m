bdcf1049de2239382fcd96558b208f05f48580f8
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-10-06 10:10:38 -0400

    Add files via upload
    better summary

--- rs_auto_test.m
+++ rs_auto_test.m
@@ -27,15 +27,21 @@ r.(rs_module).aux_outs=aux_outs;
 disp('%%%%%%%')
 disp('summary')
 disp('%%%%%%%')
+disp(sprintf('date: %s',date))
+disp(sprintf('working directory: %s',pwd))
+disp(pwd);
+ver
 rs_modules=fieldnames(r);
 for irs=1:length(rs_modules)
     rs_module=rs_modules{irs};
     diff_list=zeros(1,length(r.(rs_module).ifdif));
+    run_list=zeros(1,length(r.(rs_module).ifdif));
     for id=1:length(r.(rs_module).ifdif)
         diff_list(id)=~isempty(r.(rs_module).ifdif{id});
+        run_list(id)=~isempty(r.(rs_module).aux_outs{id});
     end
     r.(rs_module).diff_list=diff_list;
-    disp(sprintf('%20s: %3.0f tests show differences',rs_module,sum(diff_list)));
+    disp(sprintf('%20s: %3.0f tests of %3.0f show differences (%3.0f skipped in auto mode)',rs_module,sum(diff_list),sum(run_list),length(run_list)-sum(run_list)));
     if sum(diff_list)>0
         for idiff=find(diff_list>0)
             disp(sprintf(' test %2.0f:',idiff));
