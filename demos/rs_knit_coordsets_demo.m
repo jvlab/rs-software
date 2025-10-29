a773b582f5a4415bea11892beb15b9c30eeb6ed5
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-10-30 02:24:47 +0500

    Add files via upload
    added stats and plotting

--- demos/rs_knit_coordsets_demo.m
+++ demos/rs_knit_coordsets_demo.m
@@ -1,8 +1,13 @@
 % rs_knit_coordsets_demo: demonstrate knitting across datasets with partially overlapping stimuli
+% calculation of statistics, with and without scaling
+% plotting two sets of stats on same figure, tweaking stimulus names
 %
 %  See also:  RS_KNIT_COORDSETS, RS_ALIGN_COORDSETS
 %
 verbosity=getinp('pipeline display verbosity','d',[0 2],0);
+if_write=getinp('1 to write the knitted set','d',[0 1],0);
+nshuffs=getinp('number of shuffles for statistics (0 for none)','d',[0 1000],10);
+%
 filenames={'./samples/bwtextures/bgca3pt_coords_MC_sess01_10.mat','./samples/bwtextures/bdce3pt_coords_MC_sess01_10.mat','./samples/bwtextures/dgea3pt_coords_MC_sess01_10.mat'};
 nsets=length(filenames);
 aux=struct;
@@ -35,8 +40,6 @@ disp('pipeline for knitted dataset, no scaling');
 rs_showpipeline(data_knit.sets{1}.pipeline,setfields(struct(),{'fields_expand','verbosity'},{fields_expand,verbosity}));
 disp('%%%%%%%%%%%%%%%%%%%');
 %
-if getinp('write the knitted set','d',[0 1])
- end
 disp('pipeline for knitted dataset, scaling');
 rs_showpipeline(data_knit_allowscale.sets{1}.pipeline,setfields(struct(),{'fields_expand','verbosity'},{fields_expand,verbosity}));
 disp('%%%%%%%%%%%%%%%%%%%');
@@ -59,6 +62,7 @@ set(gca,'XTickLabel',typenames);
 ylabel('paradigms');
 set(gca,'YTick',[1:nsets]);
 set(gca,'YTickLabel',paradigm_names);
+drawnow;
 %
 %retrieve and plot convergence and scaling
 %
@@ -104,4 +108,48 @@ for idim=dim_list
     hold on;
     plot([0 max(niters(:))],[1 1],'k');
     title(sprintf('scale factors, dim %1.0f',idim));
+end
+drawnow;
+%
+%do statistics?
+%
+if nshuffs>0
+    aux_stats=aux;
+    aux_stats.sa_pooled=aux_align.sa_pooled;
+    aux_stats.data_align=data_align;
+    %
+    aux_stats.opts_knit.if_stats=1;
+    aux_stats.opts_knit.nshuffs=nshuffs;
+    aux_stats.opts_knit.if_plot=0; %plot locally
+    %
+    [data_knit_stats,aux_knit_stats]=rs_knit_coordsets(data_align,aux_stats); %compute stats
+    %
+    knit_stats=aux_knit_stats.knit_stats;
+    knit_stats_setup=aux_knit_stats.knit_stats_setup;
+    knit_stats_setup.dataset_labels=paradigm_names;
+%    strrep(knit_stats_setup.dataset_labels,'samples/bwtextures/',''); %shorten dataset labels
+    for k=1:length(knit_stats_setup.stimulus_labels) %thin stimulus labels
+        if mod(k,3)~=1
+            knit_stats_setup.stimulus_labels{k}='';
+        end
+    end
+    knit_stats_setup.nrows=2; %reserve two rows
+    knit_stats_setup.row=1; %plot in row 1
+    figh_stats=psg_knit_stats_plot(knit_stats,knit_stats_setup);
+    %
+    aux_allowscale_stats=aux_stats;
+    aux_allowscale_stats.opts_knit.allow_scale=1;
+    aux_allowscale_stats.opts_knit.if_normscale=1;
+    %
+    [data_knit_allowscale_stats,aux_knit_allowscale_stats]=rs_knit_coordsets(data_align,aux_allowscale_stats);
+    %
+    knit_allowscale_stats=aux_knit_allowscale_stats.knit_stats;
+    knit_stats_setup.row=2;
+    knit_stats_setup.figh=figh_stats;
+    psg_knit_stats_plot(knit_allowscale_stats,knit_stats_setup); %plot allowscale stats in second row
+end %nshuffs
+%
+%write datasets?
+%
+if if_write
 end
