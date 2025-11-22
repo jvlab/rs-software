6a8d6d84eb68254b74db9647e505982ed3b3ea2b
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-11-23 02:35:27 +0500

    Add files via upload
    allow for cell array as first argument

--- octave_compat/contains.m
+++ octave_compat/contains.m
@@ -2,14 +2,24 @@ function tf=contains(str,pat)
 % tf=contains(str,pat) is a minimal replacement for Matlab's 'contains'
 % that uses strfind
 %
-% str and pat must be strings.
+% pat must be a string.
+% str can be a string or a cell array
 %  Not supported:
-%   * str and pat as cells
 %   * pat as an array
 %   * (...,'IgnoreCase',IGNORE) syntax
 %
 % Copyright (C) J. Victor, 2025.
 %
-tf=~isempty(strfind(str,pat));
+if iscell(str)
+    str_shape=size(str);
+    str_reshaped=str(:);
+    z=zeros(1,length(str_reshaped));
+    for k=1:length(str_reshaped)
+        z(k)=~isempty(strfind(str_reshaped{k},pat));
+    end
+    tf=logical(reshape(z,str_shape));
+else
+    tf=~isempty(strfind(str,pat));
+end
 return
 end
