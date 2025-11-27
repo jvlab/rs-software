4930fc062335f8d5684cdc69ce3ceb71de7c5745
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-11-28 01:17:50 +0500

    Add files via upload
    testbed for alpha blending, incomplete

---
+++ rs_plot_style.m
@@ -0,0 +1,184 @@
+function [handles,plotstyles_used,opts_used]=rs_plot_style(coords,plotstyle,opts)
+% [handles,plotstyles_used,opts_used]=rs_plot_style(coords,plotstyle,opts) is a utility plotting
+% routine that handles marker type, marker size, line style, line
+% thickness, alpha blending, and possible conflicts or unsupported properties
+%
+% coords: a set of values to plot, either 2 or 3 columns
+%    If coords is empty, no handles are created
+% plotstyle: structure with any of the following fields (empty fields have indicated defaults)
+%   plotstyle.marker: 'none'
+%   plotstyle.markersize: 6
+%   plotstyle.linestyle: 'none'
+%   plotstyle.linewidth: 1
+%   plotstyle.color: 'k' (can also be an [r,g,b] triple)
+%   plotstyle.filled: 0 (1 to fill in)
+%   plotstyle.alpha: 1
+% opts: options, intended for hints for how to resolve conflicts
+%   opts.if_alpha_color_line: 1 (default) if line color property allows for alpha, 0 if not, -1 to determine from exist('alpha')
+%   opts.if_alpha_color_marker: 1 if marker edge color property allows for alpha, 0 (default) if not, -1 to determine from exist('alpha')
+
+%
+% handles: handle(s) to the plot, may include any of the following
+%    handles.line
+%    handles.markers
+%    handles.scatter
+%  Also includes handles.legend, the recommended handle for legends
+% plotstyles_used: plot styles used for any of the components, as well as plotstyles_used.orig, 
+%    which is plotstyles_used with defaults filled inb
+% opts_used: options used, has a msgs field
+% 
+% plot status is hold on at exit
+% if coords is empty, no handles are created
+%
+%   See also:  RS_PLOT_STYLE_TEST
+%
+if (nargin<=2)
+    opts=struct;
+end
+%line color allows for alpha, but markeredgecolor and markerfacecolor do not have alpha
+opts=filldefault(opts,'if_alpha_color_line',1);
+if opts.if_alpha_color_line==-1
+    opts.if_alpha_color_line=double(exist('alpha')>=2);
+end
+opts=filldefault(opts,'if_alpha_color_marker',0);
+if opts.if_alpha_color_marker==-1
+    opts.if_alpha_color_marker=double(exist('alpha')>=2);
+end
+%
+plotstyle_def=struct;
+plotstyle_def.marker='none';
+plotstyle_def.markersize=6;
+plotstyle_def.filled=0;
+%
+plotstyle=filldefault(plotstyle,'marker',plotstyle_def.marker);
+plotstyle=filldefault(plotstyle,'markersize',plotstyle_def.markersize);
+plotstyle=filldefault(plotstyle,'linestyle','none');
+plotstyle=filldefault(plotstyle,'linewidth',1);
+plotstyle=filldefault(plotstyle,'color','k');
+plotstyle=filldefault(plotstyle,'filled',plotstyle_def.filled);
+plotstyle=filldefault(plotstyle,'alpha',1);
+plotstyles_used=struct;
+plotstyles_used.orig=plotstyle;
+handles=[];
+opts_used=opts;
+opts_used.msgs=[];
+nds=size(coords,2);
+if ~ismember(nds,[0 2 3])
+    return
+end
+%
+handles.legend=[];
+handles.line=[];
+handles.markers=[];
+handles.scatter=[];
+%
+if plotstyle.alpha==1
+    hp=rs_plot_style_do(coords,'Line');
+    set(hp,'Color',plotstyle.color);
+    set(hp,'Marker',plotstyle.marker);
+    set(hp,'MarkerSize',plotstyle.markersize);
+    set(hp,'MarkerEdgeColor',plotstyle.color);
+    if plotstyle.filled
+        set(hp,'MarkerFaceColor',plotstyle.color);
+    end
+    set(hp,'LineStyle',plotstyle.linestyle);
+    set(hp,'LineWidth',plotstyle.linewidth);
+    handles=rs_plot_style_sethandles(handles,hp,{'legend','line','markers'});
+    return
+%now deal with alpha
+else
+    %
+    if_alpha_marker=~strcmp(plotstyle.marker,'none');
+    if_alpha_line=~strcmp(plotstyle.linestyle,'none');
+    %
+    if if_alpha_line & ~if_alpha_marker %alpha requested for line but not marker
+        hp=rs_plot_style_do(coords,'Line');
+        set(hp,'Color',plotstyle.color);
+        set(hp,'LineStyle',plotstyle.linestyle);
+        set(hp,'LineWidth',plotstyle.linewidth);
+        if opts.if_alpha_color_line
+            c3=get(hp,'Color');
+            set(hp,'Color',[c3 plotstyle.alpha]);
+        else
+            opts_used.msgs='Cannot apply alpha-blending to Line objects';
+        end
+        handles=rs_plot_style_sethandles(handles,hp,{'legend','line'});
+    end
+    if ~if_alpha_line & if_alpha_marker %alpha requested for marker but not line
+        hp=rs_plot_style_do(coords,'Scatter');
+        set(hp,'Marker',plotstyle.marker);
+        set(hp,'MarkerEdgeColor',plotstyle.color);
+        set(hp,'MarkerEdgeAlpha',plotstyle.alpha);
+        if plotstyle.filled
+            set(hp,'MarkerFaceColor',plotstyle.color);
+            set(hp,'MarkerFaceAlpha',plotstyle.alpha);
+        end
+        if (~strcmp(plotstyle.marker,plotstyle_def.marker) | (plotstyle.markersize~=plotstyle_def.markersize))
+            opts_used.msgs='Cannot customize marker or marker size while using Scatter objects for alpha-blending';
+        end
+        handles=rs_plot_style_sethandles(handles,hp,{'legend','scatter','markers'});
+    end
+
+end
+return
+end
+function hp=rs_plot_style_do(coords,plot_type)
+switch plot_type
+    case 'Line'
+        switch size(coords,2)
+            case 2
+                hp=plot(coords(:,1),coords(:,2));
+            case 3
+                hp=plot3(coords(:,1),coords(:,2),coords(:,3));
+        end       
+        set(hp,'Marker','none');
+        set(hp,'LineStyle','none');
+    case 'Scatter'
+        switch size(coords,2)
+            case 2
+                hp=scatter(coords(:,1),coords(:,2));
+            case 3
+                hp=scatter3(coords(:,1),coords(:,2),coords(:,3));
+        end
+end
+hold on;
+return
+end
+%
+function handles=rs_plot_style_sethandles(h,hp,fields)
+handles=h;
+for k=1:length(fields)
+    handles.(fields{k})=hp;
+end
+return
+end
+
+% [hplot,msg]=rs
+% 
+% 
+% function hline=rs_disp_doplot(coords,index,opts)
+% %plot the data (rows of coords) into the current plot, using index into opts.set* to determine the style
+% switch size(coords,2)
+%     case 2
+%         hline=plot(coords(:,1),coords(:,2),'k.');
+%     case 3
+%         hline=plot3(coords(:,1),coords(:,2),coords(:,3),'k.');
+% end
+% hold on;
+% index_color=1+mod(index-1,length(opts.colors));
+% set(hline,'Color',opts.colors{index_color});
+% %
+% index_marker=1+mod(index-1,length(opts.markers));
+% set(hline,'Marker',opts.markers{index_marker});
+% %
+% index_markersize=1+mod(index-1,length(opts.markersizes));
+% set(hline,'MarkerSize',opts.markersizes(index_markersize));
+% %
+% index_linestyle=1+mod(index-1,length(opts.linestyles));
+% set(hline,'LineStyle',opts.linestyles{index_linestyle});
+% %
+% index_linewidth=1+mod(index-1,length(opts.linewidths));
+% set(hline,'LineWidth',opts.linewidths(index_linewidth));
+% %
+% return
+% end
---
+++ rs_plot_style_test.m
@@ -0,0 +1,112 @@
+%rs_plot_style_test: test a utility plotting routine 
+% 
+%   See also:  RS_PLOT_STYLE
+% 
+% 
+if ~exist('opts') opts=struct; end
+%
+if ~exist('dim_list') dim_list=[2 3]; end
+if ~exist('colors') colors={'r','g','b',[.7 .3 .8]}; end
+if ~exist('marker') marker='s'; end %non-default marker
+if ~exist('markersize') markersize=12; end %non-default marker size
+if ~exist('linewidth') linewidth=3; end  %non-default line width
+if ~exist('linestyle') linestyle=':'; end %non-default line style
+if ~exist('alphaval') alphaval=0.4; end %non default alpha value
+if ~exist('fill_list') fill_list=[0 1 0 1]; end %which colors are filled in
+%
+if ~exist('npts') npts=20; end
+ncolors=length(colors);
+%
+% data are Gaussian clouds
+%
+rng('default');
+coords=randn(npts,max(dim_list),ncolors); %make Gaussian clouds
+coords=coords+repmat(randn(1,max(dim_list),ncolors),[npts 1 1]); %add offsets
+styles=cell(2,4); %rows: alpha 1 and non-default; cols: dot marker and std line, custom marker and no line, no marker and custom line, custom marker and custom line
+titles=cell(2,4);
+nrows=size(styles,1);
+ncols=size(styles,2);
+%
+handles=cell(nrows,ncols,ncolors,length(dim_list));
+plotstyles_used=cell(nrows,ncols,ncolors,length(dim_list));
+opts_used=cell(nrows,ncols,ncolors,length(dim_list));
+%
+for ir=1:nrows   
+    for ic=1:ncols
+        styles{ir,ic}=struct;
+        titles{ir,ic}='';
+        if ir==2
+            styles{ir,ic}.alpha=alphaval;
+            titles{ir,ic}=', alpha';
+        end
+        switch ic
+            case 1
+                styles{ir,ic}.marker='.';
+                styles{ir,ic}.linestyle='-';
+                titles{ir,ic}=cat(2,'std marker and line',titles{ir,ic});
+            case 2
+                styles{ir,ic}.marker=marker;
+                styles{ir,ic}.markersize=markersize;
+                titles{ir,ic}=cat(2,'cust marker',titles{ir,ic});
+            case 3
+                styles{ir,ic}.linewidth=linewidth;
+                styles{ir,ic}.linestyle=linestyle;
+                titles{ir,ic}=cat(2,'cust line',titles{ir,ic});
+            case 4
+                styles{ir,ic}.marker=marker;
+                styles{ir,ic}.markersize=markersize;
+                styles{ir,ic}.linewidth=linewidth;
+                styles{ir,ic}.linestyle=linestyle;
+                titles{ir,ic}=cat(2,'cust marker and line',titles{ir,ic});
+        end
+    end
+end
+for id=1:length(dim_list)
+    nds=dim_list(id);
+    tstring=sprintf('dim %2.0f: marker %s markersize %2.0f linestyle %s linewidth %2.0f alpha %4.2f',...
+        nds,marker,markersize,linestyle,linewidth,alphaval);
+    figure;
+    set(gcf,'Position',[50 100 1200 800]);
+    set(gcf,'NumberTitle','off');
+    set(gcf,'Name',tstring);
+    %
+    %
+    for ir=1:nrows
+        for ic=1:ncols
+            subplot(nrows,ncols,ic+ncols*(ir-1));
+            disp(cat(2,tstring,' ',titles{ir,ic}));
+            hlegend=[];
+            for icolor=1:ncolors
+                styles{ir,ic}.filled=fill_list(icolor);
+                [handles{ir,ic,icolor,id},plotstyles_used{ir,ic,icolor,id},opts_used{ir,ic,icolor,id}]=...
+                    rs_plot_style(coords(:,1:nds,icolor),setfield(styles{ir,ic},'color',colors{icolor}),opts);
+                if ~isempty(handles{ir,ic,icolor,id}.legend)
+                    hlegend(icolor)=handles{ir,ic,icolor,id}.legend;
+                else
+                    if (icolor==1)
+                        rs_warning('No legend handle returned',0);
+                    end
+                end
+                if (~isempty(opts_used{ir,ic,icolor,id}.msgs) & icolor==1)
+                    rs_warning(opts_used{ir,ic,icolor,id}.msgs,0);
+                end
+            end
+            axis equal
+            set(gca,'XLim',[-3 3]);
+            set(gca,'YLim',[-3 3]);
+            if (nds==3)
+                set(gca,'ZLim',[-3 3]);
+                axis vis3d
+                box on;
+                grid on;
+            end
+            title(titles{ir,ic});
+            if ~isempty(hlegend)
+                legend(hlegend,'Location','Best');
+            end
+        end %ic
+    end %ir
+    axes('Position',[0.01,0.02,0.01,0.01]); %for text
+    text(0,0,tstring,'Interpreter','none');
+    axis off;
+end %id
