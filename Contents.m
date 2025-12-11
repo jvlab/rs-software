2c90d01240bf814582eae3135d124364088d0602
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-12-11 06:42:37 +0500

    Add files via upload
    begin disp_enh_coordsets

--- Contents.m
+++ Contents.m
@@ -12,21 +12,26 @@
 % Data processing: coordinates
 %   rs_align_coordsets: align multiple coordinate files with non-identical stimuli
 %   rs_align_coordsets_test: test for above
+%   rs_disp_coordsets: display one or more sets of coordinates
+%   rs_disp_enh_coordsets: display coordinate sets, enhanced by coloring rays, rings, etc.
 %   rs_findrays: find rays from coordinates
 %   rs_knit_coordsets: find consensus coordinates across coordinate files with non-identical stimuli
 %   rs_knit_coordsets_test: test for above
-%   rs_xform_specify: specify a transformation (linear and translation)
+%   rs_xform_specify: specify a transformation (rotation and translation)
 %   rs_xform_specify_test: test rs_xform_specify
 %   rs_xform_apply: apply a transformation from rs_xform_specify
 %   rs_xform_specify_apply_test: test rs_xform_[specify|apply]
 %
-% Customization and testing
+% Customization, version dependence, benchmarks
 %   rs_auto_test: run all tests ion automatic mode
 %   rs_aux_customize: customize defaults for auxiliary inputs
 %   rs_aux_customize_test: test for above
 %   rs_aux_defaults_define: set up a file with default values for auxiliary parameters
 %   rs_benchmark_compare: compare a test output with benchmarks
 %   rs_check_coordsets: check consistency among several coordinate sets
+%   rs_plot_style: plot a line and points, customizing marker, color, alpha, style
+%   rs_plot_style_test: test rs_plot_style
 %   rs_template: a template for modules with an input and an output
+%   rs_warning: a standard display and tallying warnings
 % 
 %   Copyright (c) 2025 by J. Victor
---
+++ rs_disp_enh_coordsets.m
@@ -0,0 +1,126 @@
+function aux_out=rs_disp_enh_coordsets(data_in,rays,aux)
+% function aux_out=rs_disp_enh_coordsets(data_in,rays,aux)
+%
+%   See also: RS_DISP_COORDSETS, PSG_TYPENAMES2COLORS.
+%
+%  aux.opts_disp: these fields are starting points for customization for ray plots
+%                                  points     rays     rings    neighbors
+%    data_show_method:             'all'     'list'   'list'     'list' 
+%    data_label_method:            'none'*   'last'   'none'*    'none'*
+%    data_connect_method:                    'list'   'list'*    'list'
+%    connect_data_linestyles       'none'*  '-'/'--'   ':'*      '-'*
+%    set_tags                                'rays'   'rings'    'nbrs'
+%    callout_amount                           0.5*
+%    set_colors                             per ray
+%    callout_colors                         per ray*
+%   * indicates that an explicitly supplied value is NOT overridden
+%
+%need to document, what is passed through, and handles on output
+%provide options for psg_typenames2colors
+%labellig options
+%rings, rays, grids
+%need to test with multiple subsets of axes
+%
+aux=filldefault(aux,'opts_disp',struct());
+aux=filldefault(aux,'opts_disp_enh',struct());
+aux=filldefault(aux,'opts_tn2c',struct());
+%
+aux.opts_disp_enh=filldefault(aux.opts_disp_enh,'if_points',0);
+aux.opts_disp_enh=filldefault(aux.opts_disp_enh,'if_rays',1);
+aux.opts_disp_enh=filldefault(aux.opts_disp_enh,'if_rings',0);
+aux.opts_disp_enh=filldefault(aux.opts_disp_enh,'if_nbrs',0);
+%
+if aux.opts_disp_enh.if_points %plot individual points?
+    %customize standard plot options for points
+    opts_disp_points=aux.opts_disp;
+    opts_disp_points.data_show_method='all';
+    opts_disp_points=filldefault(opts_disp_points,'data_label_method','none');
+    opts_disp_points=filldefault(opts_disp_points,'connect_data_linestyles','-');
+    %
+    aux_out.points=rs_disp_coordsets(data_in,setfield(struct(),'opts_disp',opts_disp_points));
+    %update figure and axis handles so future plots are in same place
+    aux.opts_disp=rs_disp_enh_hupdate(aux.opts_disp,aux_out.points.opts_disp);
+end
+%
+if aux.opts_disp_enh.if_rays %plot points along each ra,y, in designated colors
+    for iray=1:rays.nrays
+        %customize standard plot options for rays
+        opts_disp_rays=aux.opts_disp;
+        opts_disp_rays.data_show_method='list';
+        opts_disp_rays.connect_data_method='chain';
+        opts_disp_rays=filldefault(opts_disp_rays,'data_label_method','last');
+        opts_disp_rays.set_tags='rays'; %so that this will not be in legend
+        opts_disp_rays=filldefault(opts_disp_rays,'callout_amount',0.5);
+        if_callout_colors_supplied=double(isfield(opts_disp_rays,'callout_colors'));
+        %
+        orig_ptr=min(find(rays.whichray==0));
+        for isign=-1:2:1
+            bidir=find(rays.whichray==iray);
+            mults=rays.mult(bidir);
+            sign_sel=find(sign(mults)==isign);
+            if ~isempty(sign_sel)
+                bidir_sel=bidir(sign_sel);
+                mults_sel=mults(sign_sel);
+                mb_sorted=sort([abs(mults_sel(:)),bidir_sel],1,'ascend');
+                bidir_sorted=mb_sorted(:,2); %sorted in ascending order of magnitude
+                opts_disp_rays.data_show_list=[orig_ptr bidir_sorted']; %add origin to the beginning
+                %
+                [rgb,symb,vecs,opts_used]=psg_typenames2colors(data_in.sas{1}.typenames(bidir_sorted),aux.opts_tn2c); %get standard colors and symbols
+                opts_disp_rays.set_colors{1}=rgb;
+                if ~if_callout_colors_supplied
+                    opts_disp_rays.callout_colors{1}=rgb;
+                end               
+                switch isign
+                    case 1
+                        opts_disp_rays.connect_data_linestyles='-';
+                    case -1
+                        opts_disp_rays.connect_data_linestyles='--';
+                end
+                %
+                aux_out_ray=rs_disp_coordsets(data_in,setfield(struct,'opts_disp',opts_disp_rays));
+                aux_out.rays{iray,(3+isign)/2}=aux_out_ray;
+                aux.opts_disp=rs_disp_enh_hupdate(aux.opts_disp,aux_out_ray.opts_disp);
+            end %not empty
+        end %sign
+    end %iray
+end
+%
+if aux.opts_disp_enh.if_rings %connect rings
+   for iring=1:rays.nrings
+        %customize standard plot options for rings
+        opts_disp_rings=aux.opts_disp;
+        opts_disp_rings.data_show_method='list';
+        opts_disp_rings.connect_data_method='list';
+        opts_disp_rings=filldefault(opts_disp_rings,'data_label_method','none');
+        opts_disp_rings=filldefault(opts_disp_rings,'connect_data_linestyles',':');
+        opts_disp_rings.set_tags='rings'; %so that this will not be in legend
+        ring_list=rays.rings{iring}.coord_ptrs;
+        if ~isempty(ring_list)
+            ring_list_offset=[ring_list(2:end) ring_list(1)];
+            opts_disp_rings.data_show_list=ring_list;
+            opts_disp_rings.connect_data_list=[ring_list(:),ring_list_offset(:)];
+            %
+            aux_out_ring=rs_disp_coordsets(data_in,setfield(struct,'opts_disp',opts_disp_rings));
+            aux_out.rings{iring}=aux_out_ring;
+            aux.opts_disp=rs_disp_enh_hupdate(aux.opts_disp,aux_out_ring.opts_disp);
+         end %empty?
+    end %iray
+end
+%
+aux_out.opts_disp=aux.opts_disp;
+aux_out.opts_disp_enh=aux.opts_disp_enh;
+aux_out.opts_tn2c=aux.opts_tn2c;
+return
+end
+
+function anew=rs_disp_enh_hupdate(aold,a)
+fns={'fig_handle','axis_handles'};
+for ifn=1:length(fns)
+    anew=aold;
+    fn=fns{ifn};
+    if isfield(a,fn)
+        anew.(fn)=a.(fn);
+    end
+end
+return
+end
