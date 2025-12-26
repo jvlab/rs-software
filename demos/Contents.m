52ecd6b1e080473c6a46dfc79ee40b89b92570a5
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-12-26 11:27:26 -0500

    Add files via upload
    demo2,3,4 removed and modified to be tests

--- demos/Contents.m
+++ demos/Contents.m
@@ -1,9 +1,6 @@
 % rs/demos: Representational space software demos
 %
 %   rs_disp_coordsets_demo: demo for visualization of coordinate sets: custom markers, multiple plots 
-%   rs_disp_coordsets_demo2: demo for visualization of coordinate sets: rotations, data selection, connections, offsets, labels, calloutst
-%   rs_disp_coordsets_demo3: demo for visualization of coordinate sets: synthetic data, alpha blending
-%   rs_disp_coordsets_demo4: demo for visualization of coordinate sets: rays, rings, neighbors (via rs_disp_enh_coordsets)
 %   rs_knit_coordsets_demo: demonstrates knitting together sets with partially overlapping stimuli
 % 
 %   Copyright (c) 2025 by J. Victor
