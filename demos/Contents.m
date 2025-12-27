d2642e2b1c4d2a60bb4888e4a36daa93ff2db5b6
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-12-26 22:41:46 -0500

    Add files via upload
    start demo for enhanced plotting

--- demos/Contents.m
+++ demos/Contents.m
@@ -1,6 +1,7 @@
 % rs/demos: Representational space software demos
 %
 %   rs_disp_coordsets_demo: demo for visualization of coordinate sets: custom markers, multiple plots 
+%   rs_disp_enh_coordsets_demo: demo for visualization of coordinate sets: rays, rings, offsets
 %   rs_knit_coordsets_demo: demonstrates knitting together sets with partially overlapping stimuli
 % 
 %   Copyright (c) 2025 by J. Victor
