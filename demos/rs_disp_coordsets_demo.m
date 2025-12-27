dd423cc2930d71b221f3ca929905496faa3bfb8d
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-12-26 19:25:15 -0500

    Add files via upload
    begin a more useful demo

--- demos/rs_disp_coordsets_demo.m
+++ demos/rs_disp_coordsets_demo.m
@@ -1,11 +1,9 @@
-% rs_disp_coordsets_demo: demonstrate display of datasets with several customizations
+% rs_disp_coordsets_demo: demonstrate display of several datasets and their consensus
 % 
 % data are plotted with and without rotating to consensus
 %
 % plot options illustrated:
-%  choice of data to plot
 %  choice of coordinates to plot
-%  legend placement
 %  sample datasets are rotated into a consensus, with and without scaling (in rs_knit_coordsets)
 %  combining consensus and individual datasets on same plot
 %  modification of data point colors, symbols, symbol sizes
@@ -18,182 +16,217 @@
 %  See also:  RS_DISP_COORDSETS, RS_DISP_COORDSETS_DEMO2, RS_DISP_COORDSETS_DEMO3.
 %
 filenames={'./samples/animals/image_coords_S3','./samples/animals/image_coords_S4','./samples/animals/image_coords_S5','./samples/animals/image_coords_S6'};
-nfiles=length(filenames);
+nsets=length(filenames);
 aux_in=struct;
 aux_in.opts_read=setfields(struct(),{'input_type','if_auto','if_log'},{1,1,1});
-aux_in.nsets=length(filenames);
+aux_in.nsets=nsets;
 %
 [data_read,aux_read]=rs_get_coordsets(filenames,aux_in);
-%
-%plot raw data with no customization
-%
-aux_nocust=struct;
-aux_out_nocust=rs_disp_coordsets(data_read,aux_nocust);
-%
-%now align and rotate data into a consensus, and use each component, aligned to consensus, for further plotting
-%
-aux_align_def=struct;
-[data_align,aux_align]=rs_align_coordsets(data_read,aux_align_def);
-aux_knit_def=struct;
-[data_consensus,aux_knit]=rs_knit_coordsets(data_align,aux_knit_def);
-data_components=aux_knit.components;
-%
-aux_knit_scale=struct;
-aux_knit_scale.opts_knit.allow_scale=1;
-[data_consensus_scale,aux_knit_scale]=rs_knit_coordsets(data_align,aux_knit_scale);
-data_components_scale=aux_knit_scale.components;
-%
-aux_knit_normscale=struct;
-aux_knit_normscale.opts_knit.allow_scale=1;
-aux_knit_normscale.opts_knit.if_normscale=1;
-[data_consensus_normscale,aux_knit_normscale]=rs_knit_coordsets(data_align,aux_knit_normscale);
-data_components_normscale=aux_knit_normscale.components;
-%
-%plots with 2-d projection and 3-d projections of a 5-d model
-%
-dim_select=5;
-group_size_list=[2:3];
-coord_group_methods={'all','keepone','rolling'};
-%
-opts_disp=struct;
-opts_disp.dim_select=dim_select;
-aux_out_custproj=cell(length(group_size_list),length(coord_group_methods));
-for igroup=1:length(group_size_list)
-    opts_disp.coord_group_size=group_size_list(igroup);
-    if opts_disp.coord_group_size==2
-        opts_disp.axis_scale='auto';
-    end
-    if opts_disp.coord_group_size==3
-        opts_disp.if_legend=-1;
-    end
-    for imethod=1:length(coord_group_methods)
-        opts_disp.coord_group_method=coord_group_methods{imethod};
-        opts_disp.fig_name=sprintf('consensus dim %2.0f: coords in groups of %2.0f (method: %s)',opts_disp.dim_select,opts_disp.coord_group_size,opts_disp.coord_group_method);
-        aux_out_custproj{igroup,imethod}=rs_disp_coordsets(data_components,setfield(aux_nocust,'opts_disp',opts_disp));
-    end
+subj_ids=cell(1,nsets);
+for iset=1:nsets
+    subj_ids{iset}=data_read.sets{iset}.subj_id;
 end
+nstims=data_read.sets{iset}.nstims;
+label_sparsify=6; %sparsify the labeling
+stim_label_list=[1:label_sparsify:nstims]; %label only some of the stimuli
 %
-%large  plot, adding the consensus, and showing connections to the consensus 
-% first, append the components to the consensus
-%
-%customize the display options
-opts_disp_conn=struct;
-for ifile=1:nfiles
-    opts_disp_conn.set_labels{ifile}=data_read.sets{ifile}.subj_id;
-end
-opts_disp_conn.set_labels{nfiles+1}='consensus';
-opts_disp_conn.coord_group_method='onlylowest';
-opts_disp_conn.set_markers=cellstr(strvcat(repmat('.',nfiles,1),'x'))'; %consensus plotted with x
-opts_disp_conn.connect_sets_method='star_last';
-opts_disp_conn.connect_sets_color_mode='split';
-%
-ncols=3; %noscale, scale, scale and normalize
-%3-d coord sets
+coord_groups=[1 2 3;2 3 4]; %coords to plot
+ngroups=size(coord_groups,1);
 hfig=figure;
-opts_disp_conn.dim_select=3;
-set(hfig,'Position',[100 100 1400 800]);
-set(hfig,'Name','comparison, 3 dims');
-set(hfig,'NumberTitle','off');
+opts_disp_raw=struct;
 %
-for iscale=1:ncols
-    switch iscale
-        case 1
-            data_all=data_components;
-            data_c=data_consensus;
-            subtitle='no scaling';
-        case 2
-            data_all=data_components_scale;
-            data_c=data_consensus_scale;
-            subtitle='scaling';
-        case 3
-            data_all=data_components_normscale;
-            data_c=data_consensus_normscale;
-            subtitle='scaling, normalized';
+opts_disp_raw.fig_handle=hfig;
+opts_disp_raw.fig_position=[100 100 1200 800];
+opts_disp_raw.fig_name='raw data';
+opts_disp_raw.dim_select=4;
+opts_disp_raw.coord_group_size=3;
+opts_disp_raw.coord_group_method='list';
+opts_disp_raw.coord_groups=coord_groups;
+opts_disp_raw.set_labels=subj_ids;
+opts_disp_raw.data_label_method='list';
+opts_disp_raw.data_label_list=stim_label_list;
+opts_disp_raw.callout_amount=0.3;
+opts_disp_raw.callout_colors='set_colors';
+opts_disp_raw.callout_linestyles='-';
+opts_disp_raw.legend_location='North';
+%
+aux_out_raw=cell(1,ngroups);
+haxes=cell(1,ngroups);
+for iset=1:nsets
+    for igroup=1:2 %set up subplots
+        haxes{igroup}=subplot(ngroups,nsets,iset+(igroup-1)*nsets);
     end
-    data_all.ds{nfiles+1}=data_c.ds{1};
-    data_all.sas{nfiles+1}=data_c.sas{1};
-    data_all.sets{nfiles+1}=data_c.sets{1};
-    if iscale==2
-        opts_disp_conn.if_legend=0; %show with legend suppressed
-    else
-        opts_disp_conn.if_legend=1;
-    end
-    opts_disp_conn.fig_handle=hfig;
-    opts_disp_conn.axis_handles{1}=subplot(1,ncols,iscale);
-    aux_out_conn=rs_disp_coordsets(data_all,setfield(aux_nocust,'opts_disp',opts_disp_conn));
-    subplot(1,ncols,iscale);
-    title(subtitle);
+    opts_disp_raw.axis_handles=haxes;
+    opts_disp_raw.set_select=iset;
+    aux_out_raw{iset}=rs_disp_coordsets(data_read,setfield(struct,'opts_disp',opts_disp_raw));
 end
+
+%aux_out_nocust=rs_disp_coordsets(data_read,aux_nocust);
 %
-%4-d coord sets
-hfig=figure;
-opts_disp_conn.dim_select=4;
-set(hfig,'Position',[100 100 1400 800]);
-set(hfig,'Name','comparison, 4 dims');
-set(hfig,'NumberTitle','off');
-for iscale=1:ncols
-    switch iscale
-        case 1
-            data_all=data_components;
-            data_c=data_consensus;
-            subtitle='no scaling';
-        case 2
-            data_all=data_components_scale;
-            data_c=data_consensus_scale;
-            subtitle='scaling';
-        case 3
-            data_all=data_components_normscale;
-            data_c=data_consensus_normscale;
-            subtitle='scaling, normalized';
-    end
-    data_all.ds{nfiles+1}=data_c.ds{1};
-    data_all.sas{nfiles+1}=data_c.sas{1};
-    data_all.sets{nfiles+1}=data_c.sets{1};
-    if iscale==2
-        opts_disp_conn.if_legend=0; %show with legend suppressed
-    else
-        opts_disp_conn.if_legend=1;
-    end
-    opts_disp_conn.data_label_setsel_method='last'; %label just the consensus
-    opts_disp_conn.fig_handle=hfig;
-    opts_disp_conn.axis_handles{1}=subplot(2,ncols,iscale);
-    opts_disp_conn.axis_handles{2}=subplot(2,ncols,iscale+ncols);
-    opts_disp_conn.coord_group_method='list';
-    opts_disp_conn.coord_groups=[[1 2 3];[1 2 4]];
-    aux_out_conn=rs_disp_coordsets(data_all,setfield(aux_nocust,'opts_disp',opts_disp_conn));
-    subplot(2,ncols,iscale);
-    title(subtitle);
-end
-%
-%plots with multiple customizations, just for demonstration
-%
-opts_disp2=struct;
-opts_disp2.fig_name='highly customized';
-opts_disp2.dim_select=5;
-opts_disp2.set_select=[1 3 4];
-opts_disp2.fig_position=[50 150 1400 700];
-opts_disp2.axis_font_size=9;
-opts_disp2.axis_label_font_size=7;
-opts_disp2.axis_label_prefix='d';
-opts_disp2.set_colors={'g',[.5 .5 0],[.5 0 .7],[.8 .2 .1]};
-opts_disp2.set_markers={'*','o','x','.'};
-opts_disp2.set_markersizes=[7 8 9 10];
-opts_disp2.set_labels={'s1','s2','s3','s4'};
-opts_disp2.connect_set_linestyles={':','-',':','--'};
-opts_disp2.connect_set_linewidths=[2 1 1]; %will cycle
-opts_disp2.connect_sets_method='list';
-opts_disp2.connect_sets_list=[[1 3];[3 4]];
-opts_disp2.connect_sets_color_mode='first';
-opts_disp2.coord_group_size=3;
-opts_disp2.coord_group_method='list';
-opts_disp2.coord_groups=[[1 2 3];[1 2 3];[1 2 3];[1 2 3];[1 2 3];[1 2 3];[2 3 4];[1 2 5]];
-opts_disp2.if_box=0;
-opts_disp2.axis_view={3,[-37.5 30],[-47.5 30],[-27.5 30],[-37.5 10],2,3,3};
-opts_disp2.axis_scales=3*[-1 2;-2 3;-4 5;-5 6;-6 7];
-opts_disp2.axis_scale='list';
-opts_disp2.legend_font_size=6;
-opts_disp2.legend_location='SouthEast';
-opts_disp2.data_label_method='list';
-opts_disp2.data_label_list=[1 28 37]; %label three points
-opts_disp2.data_label_setsel_method='all'; %label all datasets
-aux_out_custm=rs_disp_coordsets(data_components,setfield(aux_nocust,'opts_disp',opts_disp2));
+% %now align and rotate data into a consensus, and use each component, aligned to consensus, for further plotting
+% %
+% aux_align_def=struct;
+% [data_align,aux_align]=rs_align_coordsets(data_read,aux_align_def);
+% aux_knit_def=struct;
+% [data_consensus,aux_knit]=rs_knit_coordsets(data_align,aux_knit_def);
+% data_components=aux_knit.components;
+% %
+% aux_knit_scale=struct;
+% aux_knit_scale.opts_knit.allow_scale=1;
+% [data_consensus_scale,aux_knit_scale]=rs_knit_coordsets(data_align,aux_knit_scale);
+% data_components_scale=aux_knit_scale.components;
+% %
+% aux_knit_normscale=struct;
+% aux_knit_normscale.opts_knit.allow_scale=1;
+% aux_knit_normscale.opts_knit.if_normscale=1;
+% [data_consensus_normscale,aux_knit_normscale]=rs_knit_coordsets(data_align,aux_knit_normscale);
+% data_components_normscale=aux_knit_normscale.components;
+% %
+% %plots with 2-d projection and 3-d projections of a 5-d model
+% %
+% dim_select=5;
+% group_size_list=[2:3];
+% coord_group_methods={'all','keepone','rolling'};
+% %
+% opts_disp=struct;
+% opts_disp.dim_select=dim_select;
+% aux_out_custproj=cell(length(group_size_list),length(coord_group_methods));
+% for igroup=1:length(group_size_list)
+%     opts_disp.coord_group_size=group_size_list(igroup);
+%     if opts_disp.coord_group_size==2
+%         opts_disp.axis_scale='auto';
+%     end
+%     if opts_disp.coord_group_size==3
+%         opts_disp.if_legend=-1;
+%     end
+%     for imethod=1:length(coord_group_methods)
+%         opts_disp.coord_group_method=coord_group_methods{imethod};
+%         opts_disp.fig_name=sprintf('consensus dim %2.0f: coords in groups of %2.0f (method: %s)',opts_disp.dim_select,opts_disp.coord_group_size,opts_disp.coord_group_method);
+%         aux_out_custproj{igroup,imethod}=rs_disp_coordsets(data_components,setfield(aux_nocust,'opts_disp',opts_disp));
+%     end
+% end
+% %
+% %large  plot, adding the consensus, and showing connections to the consensus 
+% % first, append the components to the consensus
+% %
+% %customize the display options
+% opts_disp_conn=struct;
+% for ifile=1:nfiles
+%     opts_disp_conn.set_labels{ifile}=data_read.sets{ifile}.subj_id;
+% end
+% opts_disp_conn.set_labels{nfiles+1}='consensus';
+% opts_disp_conn.coord_group_method='onlylowest';
+% opts_disp_conn.set_markers=cellstr(strvcat(repmat('.',nfiles,1),'x'))'; %consensus plotted with x
+% opts_disp_conn.connect_sets_method='star_last';
+% opts_disp_conn.connect_sets_color_mode='split';
+% %
+% ncols=3; %noscale, scale, scale and normalize
+% %3-d coord sets
+% hfig=figure;
+% opts_disp_conn.dim_select=3;
+% set(hfig,'Position',[100 100 1400 800]);
+% set(hfig,'Name','comparison, 3 dims');
+% set(hfig,'NumberTitle','off');
+% %
+% for iscale=1:ncols
+%     switch iscale
+%         case 1
+%             data_all=data_components;
+%             data_c=data_consensus;
+%             subtitle='no scaling';
+%         case 2
+%             data_all=data_components_scale;
+%             data_c=data_consensus_scale;
+%             subtitle='scaling';
+%         case 3
+%             data_all=data_components_normscale;
+%             data_c=data_consensus_normscale;
+%             subtitle='scaling, normalized';
+%     end
+%     data_all.ds{nfiles+1}=data_c.ds{1};
+%     data_all.sas{nfiles+1}=data_c.sas{1};
+%     data_all.sets{nfiles+1}=data_c.sets{1};
+%     if iscale==2
+%         opts_disp_conn.if_legend=0; %show with legend suppressed
+%     else
+%         opts_disp_conn.if_legend=1;
+%     end
+%     opts_disp_conn.fig_handle=hfig;
+%     opts_disp_conn.axis_handles{1}=subplot(1,ncols,iscale);
+%     aux_out_conn=rs_disp_coordsets(data_all,setfield(aux_nocust,'opts_disp',opts_disp_conn));
+%     subplot(1,ncols,iscale);
+%     title(subtitle);
+% end
+% %
+% %4-d coord sets
+% hfig=figure;
+% opts_disp_conn.dim_select=4;
+% set(hfig,'Position',[100 100 1400 800]);
+% set(hfig,'Name','comparison, 4 dims');
+% set(hfig,'NumberTitle','off');
+% for iscale=1:ncols
+%     switch iscale
+%         case 1
+%             data_all=data_components;
+%             data_c=data_consensus;
+%             subtitle='no scaling';
+%         case 2
+%             data_all=data_components_scale;
+%             data_c=data_consensus_scale;
+%             subtitle='scaling';
+%         case 3
+%             data_all=data_components_normscale;
+%             data_c=data_consensus_normscale;
+%             subtitle='scaling, normalized';
+%     end
+%     data_all.ds{nfiles+1}=data_c.ds{1};
+%     data_all.sas{nfiles+1}=data_c.sas{1};
+%     data_all.sets{nfiles+1}=data_c.sets{1};
+%     if iscale==2
+%         opts_disp_conn.if_legend=0; %show with legend suppressed
+%     else
+%         opts_disp_conn.if_legend=1;
+%     end
+%     opts_disp_conn.data_label_setsel_method='last'; %label just the consensus
+%     opts_disp_conn.fig_handle=hfig;
+%     opts_disp_conn.axis_handles{1}=subplot(2,ncols,iscale);
+%     opts_disp_conn.axis_handles{2}=subplot(2,ncols,iscale+ncols);
+%     opts_disp_conn.coord_group_method='list';
+%     opts_disp_conn.coord_groups=[[1 2 3];[1 2 4]];
+%     aux_out_conn=rs_disp_coordsets(data_all,setfield(aux_nocust,'opts_disp',opts_disp_conn));
+%     subplot(2,ncols,iscale);
+%     title(subtitle);
+% end
+% %
+% %plots with multiple customizations, just for demonstration
+% %
+% opts_disp2=struct;
+% opts_disp2.fig_name='highly customized';
+% opts_disp2.dim_select=5;
+% opts_disp2.set_select=[1 3 4];
+% opts_disp2.fig_position=[50 150 1400 700];
+% opts_disp2.axis_font_size=9;
+% opts_disp2.axis_label_font_size=7;
+% opts_disp2.axis_label_prefix='d';
+% opts_disp2.set_colors={'g',[.5 .5 0],[.5 0 .7],[.8 .2 .1]};
+% opts_disp2.set_markers={'*','o','x','.'};
+% opts_disp2.set_markersizes=[7 8 9 10];
+% opts_disp2.set_labels={'s1','s2','s3','s4'};
+% opts_disp2.connect_set_linestyles={':','-',':','--'};
+% opts_disp2.connect_set_linewidths=[2 1 1]; %will cycle
+% opts_disp2.connect_sets_method='list';
+% opts_disp2.connect_sets_list=[[1 3];[3 4]];
+% opts_disp2.connect_sets_color_mode='first';
+% opts_disp2.coord_group_size=3;
+% opts_disp2.coord_group_method='list';
+% opts_disp2.coord_groups=[[1 2 3];[1 2 3];[1 2 3];[1 2 3];[1 2 3];[1 2 3];[2 3 4];[1 2 5]];
+% opts_disp2.if_box=0;
+% opts_disp2.axis_view={3,[-37.5 30],[-47.5 30],[-27.5 30],[-37.5 10],2,3,3};
+% opts_disp2.axis_scales=3*[-1 2;-2 3;-4 5;-5 6;-6 7];
+% opts_disp2.axis_scale='list';
+% opts_disp2.legend_font_size=6;
+% opts_disp2.legend_location='SouthEast';
+% opts_disp2.data_label_method='list';
+% opts_disp2.data_label_list=[1 28 37]; %label three points
+% opts_disp2.data_label_setsel_method='all'; %label all datasets
+% aux_out_custm=rs_disp_coordsets(data_components,setfield(aux_nocust,'opts_disp',opts_disp2));
