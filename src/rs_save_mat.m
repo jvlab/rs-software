55b3a8968e043e28af1759b959a4aab2c8183b56
Jonathan D Victor <jdvicto@med.cornell.edu>
2026-03-30 18:53:21 -0400

    rs_save documented

--- src/rs_save_mat.m
+++ src/rs_save_mat.m
@@ -1,14 +1,17 @@
 function opts_used=rs_save_mat(fullname,s,opts)
-% opts_used=rs_save_mat(fullname,s,opts) saves the fields of a structure s in a file
+% opts_used=rs_save_mat(fullname,s,opts) is a utility to save the fields of a structure s to a mat file.
+% 
+% Args:
+%   fullname (char): file name, with path; .mat will appended if necessary
+%   s (struct): a structure whose fields are to be saved
+%   opts (struct): options, with field
 %
-% fullname: file name, with path; .mat appended if need be
-% s: a structure
-% opts: options, 
-%   opts.ver: defaults to '-v7'
+%      - ver (char): format for saving; default is '-v7'
 %
+% Returns:
+%   opts_used (struct): opts, with defaults filled in
+% 
 %  See also:  PSG_SAVE_MAT.
-%
-% opts_used: options used
 %
 if (nargin<=2)
     opts=struct;
