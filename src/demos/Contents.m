db12660f21f70088058e3b43aa1c76de31f30aad
Jonathan D. Victor <jdvicto@med.cornell.edu>
2026-04-15 15:52:44 -0400

    add faces samples and demo

--- src/demos/Contents.m
+++ src/demos/Contents.m
@@ -2,6 +2,7 @@
 %
 %   rs_align_disp_coordsets_demo: alignment and visualization of coordinate sets: custom markers, multiple plots 
 %   rs_align_disp_enh_coordsets_demo: alignment and visualization of coordinate sets: rays, rings, offsets
+%   rs_disp_coordsets_demo: visualization of single coordinate sets
 %   rs_knit_coordsets_demo: knitting together sets with partially overlapping stimuli
 %   rs_toygeom_demo:  creation of toy datasets from several paradigm types, applying and fitting transformations
 %   rs_toygeom_disp:  display results of rs_toygeom_demo
