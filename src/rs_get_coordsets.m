7b8ebec488147828559bf668460f69f7e8ba5798
Jonathan D. Victor <jdvicto@med.cornell.edu>
2026-05-05 17:12:11 -0400

    change recalc of rays

--- src/rs_get_coordsets.m
+++ src/rs_get_coordsets.m
@@ -135,9 +135,11 @@ if aux_out.warn_bad==0
         psg_get_coordsets(aux.opts_read,aux.opts_rays,aux.opts_qpred,aux.nsets);
     %find rays via rs_findrays, so that coords are sought in type_coords or btc_augcoords
     for iset=1:nsets
-        [rayss{iset},wmsg,opts_rays_used{iset}]=rs_findrays(sas{iset},opts_read_used{iset}.setup_fullname,aux.opts_rays);
-        if ~isempty(wmsg)
-            aux_out=rs_warning(wmsg,0,aux_out);
+        if strcmp(sets{iset}.paradigm_type,'domain')
+            [rayss{iset},wmsg,opts_rays_used{iset}]=rs_findrays(sas{iset},opts_read_used{iset}.setup_fullname,aux.opts_rays);
+            if ~isempty(wmsg)
+                aux_out=rs_warning(wmsg,0,aux_out);
+            end
         end
     end
     %
