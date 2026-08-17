0df6fd964deac70cc6a94986f1dc93a941b0ff4a
Jonathan D. Victor <jdvicto@med.cornell.edu>
2026-08-17 11:35:53 -0400

    framework for rs_read_choicedata

---
+++ src/rs_read_choicedata.m
@@ -0,0 +1,147 @@
+function [data_comp,aux_out]=rs_read_coorddata(fullname,aux)
+% [data_comp,aux_out]=rs_read_choicedata(fullname,aux) reads a single `choice file`
+%
+% The choice file may be triadic or tetradic.  This is determined by the number of columns in the choices variable of the data file:  5 columns if triadic, 6 columns if tetradic
+% Each row holds responses from one or more presentations of a single comparison.  Comparisons may be repeated in other rows.
+% A comparison may also be repeated with a separate indexing (e.g., s1 and s2 may be reversed.
+%
+% Args:
+%   fullname (char or singleton char array): full file name with path; if empty, it will be requested interactively. File names must contain the string '_coords'. See note below regarding setup files.
+%
+%   aux (struct): a structure, can be omitted, with fields 
+%
+%     - opts_read (struct): options for reading, can be omitted, with fields
+%
+%         - if_gui (int): 1 to use graphical interface to request data file name if 'fullname' is empty, 0 to use console; default is 1; see note below regarding customization
+%         - if_log (int): 1 to log progress, 0 to omit; default is 1; see note below regarding customization
+%         - data_fullname_def (char): prompt for data file if 'data_fullname' is empty; see note below regarding customization
+%         - ui_filter (char): file name filter that appears in graphical user interface when if_gui=1; defaults to '*_coords';see note below regarding customization
+%
+%         - **Options for internal use and maintenance**
+%         - if_uselocal (int): typically omitted; 0 to use options in rs_aux_defaults, 1 is reserved for maintenance; default is 0
+%
+%     - opts_check (struct): options for consistency checking, with field
+%
+%         - if_warn (int): 1 to show warnings when datasets are checked for consistency, 0 to suppress; default is 1
+%
+% Returns:
+%   data_comp (int 2-D array): Each row contains the data from a single kind of comparison
+%
+%      - first 3 or 4 columns: indexes into the stimuli used for the comparison
+% 
+%          - triadic: col 1 is reference, col 2 is s1, col 3 is s2, comparisons are (ref,s1) and (ref,s2)
+%          - tetradic: cols 1-4 are s1-s4, comparisons are (s1,s2) and (s3,s4)
+%
+%      - next column: number of times the first comparison was judged __more similar than__ the second comparison
+%      - final column: number of times the comparison was made
+%      - that this is *opposite* from the convention in the data file.
+%
+%   aux_out (struct): auxiliary outputs and parameter values used, with fields
+%
+%     - typenames (cell array):  stimulus labels, corresponding to the indices used in the first 3 or 4 columns of choices.  The order is unchanged from the order in the data file.
+%     - warnings (char): warnings generated during consistency check
+%     - warn_bad (int): number of warnings that prevent further processing
+%     - opts_read (cell array of struct): opts_read{1} is aux.opts_read, with defaults filled in
+%     - opts_check (struct): aux.opts_check, with defaults filled in
+%
+% Note: Note regarding customization
+%     - The defaults for the following parameters can be set by editing the line containing generic.opts_read.[param_name] in  `rs_aux_defaults_define`, running it once, and saving the workspace as rs_aux_defaults.mat.
+%
+%         - if_gui
+%         - ui_filter
+%         - if_log
+%         - data_fullname_def
+%         - coord_string
+%         - type_class_aux
+%
+%  See also: RS_AUX_CUSTOMIZE, RS_CHECK_COORDSETS, PSG_READ_COORDDATA.
+%
+if (nargin<=1)
+    aux=struct;
+end
+aux=filldefault(aux,'opts_read',struct);
+%
+aux=filldefault(aux,'opts_rays',struct);
+%
+aux=filldefault(aux,'opts_check',struct);
+aux.opts_check=filldefault(aux.opts_check,'if_warn',1);
+%
+aux=rs_aux_customize(aux,'rs_read_choicedata');
+%
+data_out=struct;
+aux_out=struct;
+aux_out.warnings=[];
+aux_out.warn_bad=0;
+%
+data_comp=[];
+return
+
+%
+% if iscell(fullname)
+%     fullname=fullname{1};
+% end
+% %
+% if isempty(fullname)
+%     if aux.opts_read.if_gui
+%         if_manual=0;
+%         if aux.opts_read.if_justsetup
+%             ui_prompt='Select a setup file';
+%             ui_filter={cat(2,'*',aux.opts_read.setup_suffix,'.mat'),'setup file'};
+%         else
+%             ui_prompt='Select a coordinate file';
+%             ui_filter=aux.opts_read.ui_filter;
+%         end
+%         while (if_manual==0 & isempty(fullname))
+%             [filename_short,pathname]=uigetfile(ui_filter,ui_prompt,'Multiselect','off');
+%             if  (isequal(filename_short,0) | isequal(pathname,0)) %use Matlab's suggested way to detect cancel
+%                 if_manual=getinp('1 to return to selection from console','d',[0 1]);
+%             else
+%                 fullname=cat(2,pathname,filename_short);
+%             end
+%         end
+%     end
+% end
+% if aux.opts_read.if_justsetup==0
+%     [d,sa,opts_read_used,pipeline]=psg_read_coorddata(fullname,[],aux.opts_read);
+%     opts_read_used.data_fullnames={opts_read_used.data_fullname}; %for compatibility with rs_get_coordsets
+%     opts_read_used.setup_fullnames={opts_read_used.setup_fullname}; %for compatibility with rs_get_coordsets
+%     opts_read_used.input_type_desc=aux.opts_read.input_types{1}; %rs_read_coorddata only reads experimental data, which is type 1
+%     opts_makeset.paradigm_type_def=aux.opts_read.paradigm_type_def;
+%     opts_makeset.domain_list_def=aux.opts_read.domain_list_def;
+%     opts_makeset.if_uselocal=aux.opts_read.if_uselocal;
+%     sets=psg_make_setstruct('data',opts_read_used.dim_list,opts_read_used.data_fullname,sa.nstims,pipeline,opts_makeset);
+%     %
+%     [rays,wmsg,opts_rays_used]=rs_findrays(sa,opts_read_used.setup_fullname,aux.opts_rays);
+%     if ~isempty(wmsg)
+%         aux_out=rs_warning(wmsg,0,aux_out);
+%     end
+% else
+%     setup_fullname=fullname;
+%     [d,sa,opts_read_used,pipeline]=psg_read_coorddata(fullname,setup_fullname,aux.opts_read);
+%     d=struct;
+%     sets=struct;
+% end
+% %
+% data_out.ds{1}=d;
+% data_out.sas{1}=sa;
+% data_out.sets{1}=sets;
+% aux_out.opts_check=aux.opts_check;
+% aux_out.opts_read{1}=opts_read_used;
+% aux_out.opts_rays{1}=opts_rays_used;
+% aux_out.rayss{1}=rays;
+% %for compatibility with rs_get_coordsets;
+% aux_out.opts_qpred{1}=struct;
+% aux_out.syms_list=struct();
+% %
+% %check consistency
+% %
+% check=rs_check_coordsets(data_out,aux.opts_check);
+% if ~isempty(check.warnings) %since strvcat([],[])~=[]
+%     aux_out.warnings=strvcat(aux_out.warnings,check.warnings);
+%     warn_leadin=getfield(getfield(rs_aux_customize(struct()),'overall'),'warn_leadin');
+%     for k=1:size(aux_out.warnings,1)
+%         disp(cat(2,warn_leadin,aux_out.warnings(k,:)));
+%     end
+% end
+% aux_out.warn_bad=aux_out.warn_bad+check.warn_bad;
+% return
---
+++ src/tests/rs_read_choicedata_test.m
@@ -0,0 +1,66 @@
+% rs_read_coorddata_test: test rs_read_choicedata
+%
+%  Compares with benchmarks
+%
+%  See also:  RS_READ_CHOICEDATA, RS_BENCHMARK_COMPARE, RS_SAVE_MAT.
+%
+rs_module='read_choicedata';
+%
+%section to force btc defaults, even if rs_aux_defaults.mat has been created or modified
+if ~exist('aux_force_filename') aux_force_filename='rs_aux_defaults_btc.mat'; end
+auxs_force=struct;
+opts_needed={'opts_read','opts_check'};
+for k=1:length(opts_needed)
+    auxs_force.(opts_needed{k})=rs_aux_force(opts_needed{k},[],aux_force_filename);
+end
+%
+ntests=3;
+%
+test_descs=cell(1,ntests);
+filenames_examples=cell(1,ntests);
+auxs=cell(1,ntests);
+opts_used=cell(1,ntests);
+%
+data_outs=cell(1,ntests);
+aux_outs=cell(1,ntests);
+%
+test_descs{1}='reading triadic choice file, animal-domain';
+filenames_examples{1}={'./samples/animals/image_choices_S3.mat'};
+auxs{1}=auxs_force;
+auxs{1}.opts_read=setfields(auxs_force.opts_read,{'input_type','if_auto','if_log'},{1,1,0});
+%
+test_descs{2}='reading triadic choice file, bgca stimulus set';
+filenames_examples{2}={'./samples/bwtextures/bgca3pt_choices_MC_sess01_10.mat'};
+auxs{2}=auxs_force;
+auxs{2}.opts_read=setfields(auxs_force.opts_read,{'input_type','if_auto','if_log'},{1,1,0});
+%
+test_descs{3}='reading tetradic choice file, coordinate file, bgca-gm stimulus set';
+filenames_examples{3}={'./samples/bwtextures/bgca3pt_choices_MC-gm_sess01_10.mat'};
+auxs{3}=auxs_force;
+auxs{3}.opts_read=setfields(auxs_force.opts_read,{'input_type','if_auto','if_log'},{1,1,0});
+%
+fns=cell(1,ntests);
+ifdif=cell(1,ntests);
+for itest=1:ntests
+    disp(sprintf('testing rs_%s: %s',rs_module,test_descs{itest}));
+    [data_outs{itest},aux_outs{itest}]=rs_read_choicedata(filenames_examples{itest},auxs{itest});
+    fns{itest}=sprintf('rs_%s_test_%1.0f',rs_module,itest);
+    s=struct;
+    s.data_out=data_outs{itest};
+    s.aux_out=aux_outs{itest};
+    rs_save_mat(cat(2,'tests',filesep,fns{itest}),s);
+end
+%
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+%
+for itest=1:ntests
+    if ~isempty(data_outs{itest})
+        disp(sprintf('testing rs_%s: %s',rs_module,test_descs{itest}));
+        [ifdif{itest},opts_used{itest}]=rs_benchmark_compare(fns{itest});
+        if ~isempty(aux_outs{itest}.warnings)
+            disp('warnings encountered during test:')
+            disp(aux_outs{itest}.warnings)
+        end
+    end
+end
+
