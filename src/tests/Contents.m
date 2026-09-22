946f0daa2f6b64f9ce5e5d8eaf17bd60ba9f2715
Jonathan D. Victor <jdvicto@med.cornell.edu>
2026-09-22 09:44:45 -0400

    start addtree

--- src/tests/Contents.m
+++ src/tests/Contents.m
@@ -1,18 +1,27 @@
-% Representational space software tests
+% Representational spaces software tests
 %
-% Individual testing modules
-% rs_align_coordsets_test: test rs_align_coordsets 
+% Individual testing modules: read/write and utils
 % rs_aux_customize_test: test rs_aux_customize
+% rs_get_coordsets_test: test rs_get_coordsets
+% rs_import_coordsets_test: test rs_import_coordsets
+% rs_read_choicedata_test: test rs_read_choicedata
+% rs_read_coorddata_test: test rs_read_coorddata
+%
+% Individual testing modules: representational spaces
+% rs_align_coordsets_test: test rs_align_coordsets 
 % rs_disp_coordsets_test1: test rs_disp_coordsets    
 % rs_disp_coordsets_test2: test rs_disp_coordsets
 % rs_disp_coordsets_test3: test rs_disp_coordsets
 % rs_geofit_test: test rs_geofit
-% rs_get_coordsets_test: test rs_get_coordsets
-% rs_import_coordsets_test: test rs_import_coordsets
 % rs_plot_style_test: test test rs_plot_style
-% rs_read_coorddata_test: test rs_read_coorddata
+% rs_vara_coordsets_test: test rs_vara_coordsets
 % rs_xform_specify_apply_test: test rs_xform_specify and rs_xform_apply
 % rs_xform_specify_test: test rs_xform_specify
+%
+% Individual testing modules: choice probabilities
+% rs_addtree_choicedata_test: test rs_addtree_choicedata
+% rs_dirfit_choicedata_test: test rs_dirfit_choicedata
+% rs_symumi_choicedata_test: test rs_rs_symumi_choicedata
 %
 % Supporting modules
 % rs_auto_test: run all tests
